09ea872ce1e05a920ce304bed7fa2d1b621fee67
YoungCreator <sashabezrukovownmail@gmail.com>
2019-09-22 23:26:07 +0300

    Initial commit

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,180 @@
+// Copyright IBM Corp. 2018,2019. All Rights Reserved.
+// Node module: loopback4-example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {repository} from "@loopback/repository";
+import {validateCredentials} from "../services/validator";
+import {post, param, get, requestBody, HttpErrors} from "@loopback/rest";
+import {User} from "../models";
+import {UserRepository} from "../repositories";
+import {inject} from "@loopback/core";
+import {
+  authenticate,
+  TokenService,
+  UserService,
+} from "@loopback/authentication";
+import {UserProfile, securityId, SecurityBindings} from "@loopback/security";
+import {
+  CredentialsRequestBody,
+  ChangeIdRequestBody,
+  UserProfileSchema,
+} from "./specs/user-controller.specs";
+import {Credentials, CredentialsForChangeId} from "../repositories/user.repository";
+import {PasswordHasher} from "../services/hash.password.bcryptjs";
+
+import {
+  TokenServiceBindings,
+  PasswordHasherBindings,
+  UserServiceBindings,
+} from "../keys";
+import * as _ from "lodash";
+import { UserServiceForPatch } from "../services/user-service";
+
+export class UserController {
+  constructor(
+    @repository(UserRepository) public userRepository: UserRepository,
+    @inject(PasswordHasherBindings.PASSWORD_HASHER)
+    public passwordHasher: PasswordHasher,
+    @inject(TokenServiceBindings.TOKEN_SERVICE)
+    public jwtService: TokenService,
+    @inject(UserServiceBindings.USER_SERVICE)
+    public userService: UserService<User, Credentials>,
+    @inject(UserServiceBindings.USER_SERVICE_FOR_PATCHING)
+    public userServicePatching: UserServiceForPatching
+  ) {}
+
+  @post("/users", {
+    responses: {
+      "200": {
+        description: "User",
+        content: {
+          "application/json": {
+            schema: {
+              "x-ts-type": User,
+            },
+          },
+        },
+      },
+    },
+  })
+  async create(@requestBody() user: User): Promise<User> {
+    // ensure a valid email value and password value
+    validateCredentials(_.pick(user, ["email", "password"]));
+
+    // encrypt the password
+    // eslint-disable-next-line require-atomic-updates
+    user.password = await this.passwordHasher.hashPassword(user.password);
+
+    try {
+      // create the new user
+      const savedUser = await this.userRepository.create(user);
+      delete savedUser.password;
+
+      return savedUser;
+    } catch (error) {
+      // MongoError 11000 duplicate key
+      if (error.code === 11000 && error.errmsg.includes("index: uniqueEmail")) {
+        throw new HttpErrors.Conflict("Email value is already taken");
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  @get("/users/{userId}", {
+    responses: {
+      "200": {
+        description: "User",
+        content: {
+          "application/json": {
+            schema: {
+              "x-ts-type": User,
+            },
+          },
+        },
+      },
+    },
+  })
+  async findById(@param.path.string("userId") userId: string): Promise<User> {
+    return this.userRepository.findById(userId, {
+      fields: {password: false},
+    });
+  }
+
+  @get("/users/me", {
+    responses: {
+      "200": {
+        description: "The current user profile",
+        content: {
+          "application/json": {
+            schema: UserProfileSchema,
+          },
+        },
+      },
+    },
+  })
+  @authenticate("jwt")
+  async printCurrentUser(
+    @inject(SecurityBindings.USER)
+    currentUserProfile: UserProfile,
+  ): Promise<UserProfile> {
+    // (@jannyHou)FIXME: explore a way to generate OpenAPI schema
+    // for symbol property
+    currentUserProfile.id = currentUserProfile[securityId];
+    delete currentUserProfile[securityId];
+    return currentUserProfile;
+  }
+
+  @post("/users/login", {
+    responses: {
+      "200": {
+        description: "Token",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                token: {
+                  type: "string",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  async login(
+    @requestBody(CredentialsRequestBody) credentials: Credentials,
+  ): Promise<{token: string}> {
+    // ensure the user exists, and the password is correct
+    const user = await this.userService.verifyCredentials(credentials);
+
+    // convert a User object into a UserProfile object (reduced set of properties)
+    const userProfile = this.userService.convertToUserProfile(user);
+
+    // create a JSON Web Token based on the user profile
+    const token = await this.jwtService.generateToken(userProfile);
+
+    return {token};
+  }
+
+  @patch("/users/changeid", {
+    responsed: {
+      "200": {
+        description: "Change user tag",
+        content: {
+          "application/json": {
+            schema: UserProfileSchema,
+          },
+        },
+      },
+    },
+  })
+  async changeId(
+    @requestBody(ChangeIdRequestBody) credentials: CredentialsForChangeId,
+  ): Promise<User> {
+    const id = await this.userService.
+  }
+}
---
+++ src/services/user-service-patching.ts
@@ -0,0 +1,10 @@
+import {HttpErrors} from "@loopback/rest";
+import {
+  CredentialsForChangeId,
+  UserRepository,
+} from "../repositories/user.repository";
+
+//TODO: Move class UserServiceForPatching from user-service.ts
+// and create interface for this class
+// then create bindings in keys.ts
+// and bind it in application.ts
