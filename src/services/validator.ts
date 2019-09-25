060126430ea53847035cde70a0ebb21b297ff94d
YoungCreator <sashabezrukovownmail@gmail.com>
2019-09-26 00:19:41 +0300

    .

--- src/services/validator.ts
+++ src/services/validator.ts
@@ -3,20 +3,20 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {Credentials} from '../repositories/user.repository';
-import * as isemail from 'isemail';
-import {HttpErrors} from '@loopback/rest';
+import {Credentials} from "../repositories/user.repository";
+import * as isemail from "isemail";
+import {HttpErrors} from "@loopback/rest";
 
 export function validateCredentials(credentials: Credentials) {
   // Validate Email
   if (!isemail.validate(credentials.email)) {
-    throw new HttpErrors.UnprocessableEntity('invalid email');
+    throw new HttpErrors.UnprocessableEntity("Invalid email");
   }
 
   // Validate Password Length
   if (credentials.password.length < 8) {
     throw new HttpErrors.UnprocessableEntity(
-      'password must be minimum 8 characters',
+      "Password must be minimum 8 characters",
     );
   }
 }
